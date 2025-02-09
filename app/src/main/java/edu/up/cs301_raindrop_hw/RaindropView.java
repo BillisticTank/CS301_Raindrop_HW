c694a8819d6d46e822ec7866a034dbd5aa51d3af
Alexander <leah28@up.edu>
2025-02-08 19:13:53 -0800

    raindrop innitial commit

---
+++ app/src/main/java/edu/up/cs301_raindrop_hw/RaindropView.java
@@ -0,0 +1,37 @@
+package edu.up.cs301_raindrop_hw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+
+public class RaindropView extends SurfaceView {
+
+    Paint blue = new Paint();
+
+    public RaindropView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setWillNotDraw(false);
+
+        this.blue.setColor(0xFF0000FF);
+        this.blue.setStyle(Paint.Style.FILL);
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        canvas.drawOval(800.0f, 400.0f, 450.0f, 820.0f, this.blue);
+    }
+}
+
+/**
+ External Citation
+ Date: 28 January 2025
+ Problem: Couldnt figure out how to start drawing raindrops
+ Resource:
+ Prof. Nuxoll - Tuesday Office Hours
+ **/
