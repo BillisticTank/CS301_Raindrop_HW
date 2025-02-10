a6aa27db22fe9fbd865bc0c6ba31142b9c74faad
Alexander <leah28@up.edu>
2025-02-10 15:57:40 -0800

    Adds a for loop to draw in between 6 and 12 raindrops. also adds a method for random paints

--- app/src/main/java/edu/up/cs301_raindrop_hw/Raindrop.java
+++ app/src/main/java/edu/up/cs301_raindrop_hw/Raindrop.java
@@ -1,46 +1,71 @@
 package edu.up.cs301_raindrop_hw;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-
 import java.util.Random;
 
 public class Raindrop
 {
-
+    // new object of the random class. Used to make random values
     Random randomness = new Random();
-    protected int xPos;
-    protected int yPos;
-    protected int rainDropSize = 30;
-    protected Paint blueness;
 
+    // x position variable
+    protected int xPos;
+
+    // y position varaible
+    protected int yPos;
+
+    //sets the size of raindrop sas a variable
+    protected int rainDropSize = 30;
+
+    //new paint object for making each circle have a unique color
+    protected Paint uniquePaint;
+
+    /** defualt constructer for the Raindrop(each individual circle drawn)
+     * this contructer calls the random position and color methods to meet random position
+     * and unique color requirments
+     */
     public Raindrop()
     {
         randomPos(xPos, yPos);
-        this.blueness.setColor(0xFF0000FF);
-        this.blueness.setStyle(Paint.Style.FILL);
+        randomPaint();
+
     }
+
     /**
-    public Raindrop(int _x, int _y)
+     * This method makes every circle have a unique color by using the randomness object
+     * to generate values in between 256 and zero, making it so each circle has a random color
+     */
+    public void randomPaint()
     {
-        xPos = _x;
-        yPos = _y;
-        this.blueness.setColor(0xFF0000FF);
-        this.blueness.setStyle(Paint.Style.FILL);
+        int _randomPaint = Color.rgb(randomness.nextInt(256),randomness.nextInt(256),randomness.nextInt(256));
+
+        uniquePaint = new Paint();
+        uniquePaint.setColor(_randomPaint);
     }
-     **/
+
+    /**
+     * This method makes every circle have a unique, random position on the surface view in between
+     * 800 pixles and 0 pixels
+     */
     public void randomPos(int _xPos, int _yPos)
     {
-        _xPos = randomness.nextInt(500) + 5;
-        _yPos = randomness.nextInt(500) + 5;
+        _xPos = randomness.nextInt(800);
+        _yPos = randomness.nextInt(800);
 
         xPos = _xPos;
         yPos = _yPos;
     }
 
+    /**
+     *  this method acts to actually draw the individual raindrops (circles) using all the data
+     *  generated from the previous variables
+     * @param canvas
+     */
     public void placement(Canvas canvas)
     {
-        canvas.drawCircle(xPos, yPos, rainDropSize, this.blueness);
+        canvas.drawCircle(xPos, yPos, rainDropSize, uniquePaint);
     }
 
 }
@@ -52,4 +77,13 @@ public class Raindrop
  Resource:
  Prof. Nuxoll - thursday 2/6/2025 lecture where the class learned how to draw spots with random
  x and y positions
+ **/
+
+/**
+ External Citation
+ Date: 10 Febuary 2025
+ Problem: Android app wouldnt run on the tablet due to fatal exception(this was due to a object call
+ to an object that didnt exist)
+ Resource:
+ Prof. Libby - Monday office hours
  **/
--- app/src/main/java/edu/up/cs301_raindrop_hw/RaindropView.java
+++ app/src/main/java/edu/up/cs301_raindrop_hw/RaindropView.java
@@ -1,25 +1,51 @@
 package edu.up.cs301_raindrop_hw;
 
 import android.content.Context;
+
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
-
+import java.util.Random;
 
 public class RaindropView extends SurfaceView
 {
-    public RaindropView(Context context, AttributeSet attrs) {
+    /**
+     * this is a new object of random class to generate random numbers
+     */
+    Random amount = new Random();
+
+    /**
+     * This contructer allows drawing to happen
+     * @param context
+     * @param attrs
+     */
+    public RaindropView(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
+
+        /**
+         * This is absolutley NECASSARY for anything to be drawn
+         */
         this.setWillNotDraw(false);
     }
 
+    /**
+     * This method actually draws each raindrop using a for loop that makes between 6 and 12 raindrops
+     * to satify assignment requirments
+     * @param _canvas the canvas on which the background will be drawn
+     */
     @Override
     public void onDraw(Canvas _canvas)
     {
-        Raindrop rain = new Raindrop();
-        rain.placement(_canvas);
+        for(int i = 0; i < amount.nextInt(12) + 6; i++)
+        {
+            Raindrop rain = new Raindrop();
+            rain.placement(_canvas);
+        }
+        
 
     }
 }
